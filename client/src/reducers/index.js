05ba2a037fa6b010e82aaa1ccf63bc7f8d91c222
Jonathan May <jmay4826@gmail.com>
2017-10-30 15:50:44 -0500

    get vehicles action and reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,13 @@
+import { combineReducers } from "redux";
 import axios from "axios";
 
-export const LOGIN = "login";
+const rootReducer = combineReducers({
+  login: loginReducer,
+  vehicles: vehiclesReducer
+});
+
+export const LOGIN = "LOGIN";
+export const GET_VEHICLES = "GET_VEHICLES";
 const ROOT_URL = "";
 
 ///Action creator
@@ -13,9 +20,18 @@ export function loginAction(user) {
   };
 }
 
+export function getVehiclesAction() {
+  const request = axios.get(`${ROOT_URL}/api/cars`);
+
+  return {
+    type: GET_VEHICLES,
+    payload: request
+  };
+}
+
 ///Reducer
 
-export default function loginReducer(state = { user: "" }, action) {
+export function loginReducer(state = {}, action) {
   switch (action.type) {
     case LOGIN + "_FULFILLED":
       return Object.assign({}, state, { username: action.payload.username });
@@ -30,3 +46,21 @@ export default function loginReducer(state = { user: "" }, action) {
       return state;
   }
 }
+
+export function vehiclesReducer(state = {}, action) {
+  switch (action.type) {
+    case GET_VEHICLES + "_FULFILLED":
+      return Object.assign({}, state, { vehicles: action.payload.data });
+    case GET_VEHICLES + "_REJECTED":
+      return Object.assign({}, state, {
+        error: action.payload.response.status
+      });
+    case GET_VEHICLES + "_PENDING":
+      return Object.assign({}, state, { loading: true });
+    default:
+      console.log("uncaught action", action);
+      return state;
+  }
+}
+
+export default rootReducer;
