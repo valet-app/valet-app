2def0b0b4a2093a1b2b0d9d64f1da3397fed1509
James Scott <scottj1426@gmail.com>
2017-10-30 15:54:52 -0500

    all routes added

---
+++ client/src/components/carParked/carParked.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+class CarParked extends Component{
+
+    render(){
+        return (
+            <div>
+                Park a Car with "COMPLETE" button!
+                </div>
+        )
+    }
+}
+export default  CarParked;
---
+++ client/src/components/getCar/getCar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+class GetCar extends Component{
+
+    render(){
+        return (
+            <div>
+                Retrieve a car with "Retrieve this car" button 
+                </div>
+        )
+    }
+}
+export default  GetCar;
