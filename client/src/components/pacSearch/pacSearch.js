34f7aab6eb95478b4ebcc7edb0de17afe51be19b
Aminata Foon <aminatafoon@Aminatas-MacBook-Air.local>
2017-10-31 15:02:18 -0500

    lets search

--- client/src/components/pacSearch/pacSearch.js
+++ client/src/components/pacSearch/pacSearch.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { Search, Grid, Button, Label } from "semantic-ui-react";
+import { Search, Grid, Button, Label, Segment } from "semantic-ui-react";
 import PropTypes from "prop-types";
 
 import _ from "lodash";
