cea370c8494dd3a227c4545110c53594a9964c1a
Aminata Foon <aminatafoon@Aminatas-MacBook-Air.local>
2017-11-02 10:25:04 -0500

    login errors

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -31,6 +31,7 @@ class Login extends Component {
       },
       () => this.props.history.push("/home")
     );
+    console.log(this.state)
   }
   render() {
     const loginError = this.props.login.error;
