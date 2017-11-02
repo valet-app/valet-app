6a9d26df59ab9cf45ec19bf74e50fdbe62690dc8
Jonathan May <jmay4826@gmail.com>
2017-11-01 23:49:00 -0500

    login redirect with session, clean up get/park logic on pacSearch, add get functionality to parkCar

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { getUserSessionAction } from "./reducers";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Login from "./components/login/login";
+import ValetOptions from "./components/valetGaragePage/valetGarage";
+import PacSearch from "./components/pacSearch/pacSearch";
+import ChooseValet from "./components/chooseValet/chooseValet";
+import ParkCar from "./components/parkCar/parkCar";
+import CarParked from "./components/carParked/carParked";
+import GetCar from "./components/getCar/getCar";
+import GetCarSearch from "./components/getCarSearch/getCarSearch";
+import CarDelivered from "./components/carDelivered/carDelivered";
+import SignupCompany from "./components/signupCompany/signupCompany";
 
 class App extends Component {
+  componentWillMount() {
+    if (!this.props.login.username) {
+      console.log(this.props);
+      this.props.getUserSessionAction();
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        {this.props.login.username || this.props.session.username ? (
+          <Switch>
+            <Route path="/get/complete" component={CarDelivered} />
+            <Route path="/get/start" component={ParkCar} />
+            <Route path="/park/complete" component={CarParked} />
+            <Route path="/park/start" component={ParkCar} />
+            <Route path="/get/chooseValet" component={ChooseValet} />
+            <Route path="/park/chooseValet" component={ChooseValet} />
+            <Route path="/park/search" component={PacSearch} />
+            <Route path="/get/search" component={PacSearch} />
+            <Route path="/home" component={ValetOptions} />
+            <Route path="/signupCompany" component={SignupCompany} />
+            <Route path="/" component={ValetOptions} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        ) : (
+          <Route path="/" component={Login} />
+        )}
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => state;
+export default connect(mapStateToProps, { getUserSessionAction })(App);
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import promise from "redux-promise-middleware";
 import "./semantic/dist/semantic.min.css";
 
@@ -12,37 +12,12 @@ import reducers from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
 
 //imported components - James
-import Login from "./components/login/login";
-import ValetOptions from "./components/valetGaragePage/valetGarage";
-import PacSearch from "./components/pacSearch/pacSearch";
-import ChooseValet from "./components/chooseValet/chooseValet";
-import ParkCar from "./components/parkCar/parkCar";
-import CarParked from "./components/carParked/carParked";
-import GetCar from "./components/getCar/getCar";
-import GetCarSearch from "./components/getCarSearch/getCarSearch";
-import CarDelivered from "./components/carDelivered/carDelivered";
-import SignupCompany from "./components/signupCompany/signupCompany";
 
 const createStoreWithMiddelware = applyMiddleware(promise())(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddelware(reducers)}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/get/complete" component={CarDelivered} />
-        <Route path="/get/start" component={ParkCar} />
-        <Route path="/park/complete" component={CarParked} />
-        <Route path="/park/start" component={ParkCar} />
-        <Route path="/get/chooseValet" component={ChooseValet} />
-        <Route path="/park/chooseValet" component={ChooseValet} />
-        <Route path="/park/search" component={PacSearch} />
-        <Route path="/get/search" component={PacSearch} />
-        <Route path="/home" component={ValetOptions} />
-        <Route path="/login" component={Login} />
-        <Route path="/signupCompany" component={SignupCompany} />
-        <Route path="/" component={App} />
-      </Switch>
-    </BrowserRouter>
+    <App />
   </Provider>,
   document.getElementById("root")
 );
