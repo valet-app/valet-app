7b8ab058966397ff6c7675f0e1a7ddc459211ca9
Jonathan May <jmay4826@gmail.com>
2017-11-06 15:37:53 -0600

    lot status works

--- client/src/components/lotStatus/lotStatus.js
+++ client/src/components/lotStatus/lotStatus.js
@@ -1,53 +1,108 @@
 import React, { Component, Dropdown } from "react";
-import { Tab, Divider, Grid } from "semantic-ui-react";
-import { getLotStatusAction } from "../../reducers";
+import { Tab, Divider, Grid, Accordion, Icon } from "semantic-ui-react";
+import { getLotStatusAction, setNavTitleAction } from "../../reducers";
 import { connect } from "react-redux";
+import NavBar from "../navBar/navBar";
 import _ from "lodash";
-
-// const panes = [
-//     { menuItem: '{}', render: () => <Tab.Pane attached={false}>Tab 1 Content</Tab.Pane> },
-//     { menuItem: 'Tab 2', render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane> },
-//     { menuItem: 'Tab 3', render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane> },
-//   ]
 
 class LotStatus extends Component {
   constructor(props) {
     super(props);
     this.props.getLotStatusAction();
-    this.state = {};
+    this.state = { activeIndex: 0 };
+    this.props.setNavTitleAction("Dashboard", () =>
+      this.props.history.push("/home")
+    );
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({ lotStatus: nextProps.lotStatus });
   }
 
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    console.log(index, activeIndex, newIndex);
+    this.setState({ activeIndex: newIndex });
+  };
   render() {
-    console.log(this.state.lotStatus);
-    const lots = this.props.lotStatus.map(lot => lot.location1);
-    const tabNames = _.uniq(lots);
+    const spaces = {};
+    this.props.lotStatus.sort().forEach(space => {
+      if (spaces[space.location1]) {
+        spaces[space.location1].push(space);
+      } else {
+        spaces[space.location1] = [space];
+      }
+    });
+    const { activeIndex } = this.state;
 
-    console.log(tabNames);
+    let spacesJsx = [];
+    let i = 0;
+    for (var key in spaces) {
+      let locationJsx = spaces[key]
+        .sort(
+          (a, b) =>
+            +a.location5.replace(/[^0-9]/g, "") -
+            +b.location5.replace(/[^0-9]/g, "")
+        )
+        .map(space => (
+          <div>
+            {space.location2} {space.location5} -
+            <span
+              style={{
+                color: space.parkingstatus ? "rgb(200, 0, 0)" : "rgb(0, 150, 0)"
+              }}
+            >
+              {space.parkingstatus ? space.parkingstatus : "Open"}
+            </span>
+          </div>
+        ));
+
+      spacesJsx.push(
+        <div key={key}>
+          <Accordion.Title
+            active={activeIndex === key}
+            index={key}
+            onClick={this.handleClick}
+          >
+            <Icon name="dropdown" />
+            {key}
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === key}>
+            {locationJsx}
+          </Accordion.Content>
+        </div>
+      );
+      i++;
+    }
+
     return (
-      <Grid centered className="white"
-      computer={6}
-      mobile={12}
-      verticalAlign="middle">
-          <Grid.Column verticalAlign="middle" width={12}>
-        {tabNames[2]}
-        </Grid.Column>
-        <Grid.Column verticalAlign="middle" width={12} 
+      <div>
+        <NavBar />
+        <Grid
+          centered
+          className="white"
+          computer={6}
+          mobile={12}
+          verticalAlign="middle"
         >
-        {tabNames[3]}
-        </Grid.Column>
-        <Grid.Column verticalAlign="middle" width={12}>
-        {tabNames[0]}
-        </Grid.Column>
-        <Grid.Column verticalAlign="middle" width={12}>
-        {tabNames[1]}
-        </Grid.Column>
-      </Grid>
+          <Grid.Column verticalAlign="middle" width={12}>
+            <Accordion>
+              {spacesJsx.sort((a, b) => {
+                return (
+                  +a.key.replace(/[^0-9]/g, "") - +b.key.replace(/[^0-9]/g, "")
+                );
+              })}
+            </Accordion>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
 const mapStateToProps = state => state;
-export default connect(mapStateToProps, { getLotStatusAction })(LotStatus);
+export default connect(mapStateToProps, {
+  getLotStatusAction,
+  setNavTitleAction
+})(LotStatus);
