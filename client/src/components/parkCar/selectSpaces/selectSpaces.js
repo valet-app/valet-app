220a59d3730f4be1ae9d338edc146c653873d481
Jonathan May <jmay4826@gmail.com>
2017-11-08 11:12:21 -0600

    spaces works

--- client/src/components/parkCar/selectSpaces/selectSpaces.js
+++ client/src/components/parkCar/selectSpaces/selectSpaces.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Dropdown, Step } from "semantic-ui-react";
+import { Dropdown, Step, Header } from "semantic-ui-react";
 import _ from "lodash";
 import { connect } from "react-redux";
 import { chooseSpaceAction } from "../../../reducers";
@@ -14,6 +14,7 @@ class SelectSpaces extends Component {
       selectedLocation4: 0,
       selectedLocation5: 0
     };
+    console.log(this.props.chosenVehicle);
   }
 
   render() {
@@ -24,9 +25,21 @@ class SelectSpaces extends Component {
       selectedLocation4,
       selectedLocation5
     } = this.state;
-    const { openSpaces } = this.props;
+    const { openSpaces, chosenVehicle } = this.props;
+    let location1, location2, location3, location4, location5;
     if (openSpaces.length) {
-      let location1 = _.sortBy(
+      if (chosenVehicle.parkingspace_id && this.state.changed) {
+        openSpaces.push({
+          location1: chosenVehicle.location1,
+          location2: chosenVehicle.location2,
+          location3: chosenVehicle.location3,
+          location4: chosenVehicle.location4,
+          location5: chosenVehicle.location5,
+          id: chosenVehicle.parkingspace_id
+        });
+      }
+
+      location1 = _.sortBy(
         _.uniqBy(openSpaces, "location1"),
         "location1"
       ).map((space, index) => {
@@ -35,7 +48,7 @@ class SelectSpaces extends Component {
           value: index
         });
       });
-      let location2 = _.sortBy(
+      location2 = _.sortBy(
         _.uniqBy(
           openSpaces.filter(
             (space, index) =>
@@ -52,7 +65,7 @@ class SelectSpaces extends Component {
         });
       });
 
-      let location3 = _.sortBy(
+      location3 = _.sortBy(
         _.uniqBy(
           openSpaces.filter(
             (space, index) =>
@@ -70,7 +83,7 @@ class SelectSpaces extends Component {
         });
       });
 
-      let location4 = _.sortBy(
+      location4 = _.sortBy(
         _.uniqBy(
           openSpaces.filter(
             (space, index) =>
@@ -89,7 +102,7 @@ class SelectSpaces extends Component {
         });
       });
 
-      let location5 = _.sortBy(
+      location5 = _.sortBy(
         _.uniqBy(
           openSpaces.filter(
             (space, index) =>
@@ -108,77 +121,130 @@ class SelectSpaces extends Component {
           id: space.id
         });
       });
+
+      if (chosenVehicle.parkingspace_id && !this.state.changed) {
+        location1.unshift(
+          Object.assign({}, this.props.chosenVehicle, {
+            text: this.props.chosenVehicle.location1,
+            value: 0,
+            id: this.props.chosenVehicle.parkingspace_id
+          })
+        );
+        location2.unshift(
+          Object.assign({}, this.props.chosenVehicle, {
+            text: this.props.chosenVehicle.location2,
+            value: 0,
+            id: this.props.chosenVehicle.parkingspace_id
+          })
+        );
+        location3.unshift(
+          Object.assign({}, this.props.chosenVehicle, {
+            text: this.props.chosenVehicle.location3,
+            value: 0,
+            id: this.props.chosenVehicle.parkingspace_id
+          })
+        );
+        location4.unshift(
+          Object.assign({}, this.props.chosenVehicle, {
+            text: this.props.chosenVehicle.location4,
+            value: 0,
+            id: this.props.chosenVehicle.parkingspace_id
+          })
+        );
+        location5.unshift(
+          Object.assign({}, this.props.chosenVehicle, {
+            text: this.props.chosenVehicle.location5,
+            value: 0,
+            id: this.props.chosenVehicle.parkingspace_id
+          })
+        );
+      }
       this.props.chooseSpaceAction(location5[selectedLocation5].id);
     }
     return (
       <div>
-        <Dropdown
-          options={location1}
-          value={selectedLocation1}
-          inline
-          onChange={(e, data) => {
-            this.setState({
-              selectedLocation1: data.value,
-              selectedLocation2: 0,
-              selectedLocation3: 0,
-              selectedLocation4: 0,
-              selectedLocation5: 0
-            });
-            this.props.chooseSpaceAction(location5[selectedLocation5]);
-          }}
-        />
+        {location5 ? (
+          <div>
+            <Header as="h2">
+              {location1[selectedLocation1].text} -{" "}
+              {location5[selectedLocation5].text}
+            </Header>
+            <Dropdown
+              options={location1}
+              value={selectedLocation1}
+              inline
+              onChange={(e, data) => {
+                this.setState({
+                  selectedLocation1: data.value,
+                  selectedLocation2: 0,
+                  selectedLocation3: 0,
+                  selectedLocation4: 0,
+                  selectedLocation5: 0,
+                  changed: true
+                });
+                this.props.chooseSpaceAction(location5[selectedLocation5]);
+              }}
+            />
 
-        <Dropdown
-          value={selectedLocation2}
-          inline
-          onChange={(e, data) => {
-            console.log("change");
-            this.setState({
-              selectedLocation2: data.value,
-              selectedLocation3: 0,
-              selectedLocation4: 0,
-              selectedLocation5: 0
-            });
-            this.props.chooseSpaceAction(location5[selectedLocation5]);
-          }}
-          options={location2}
-        />
-        <Dropdown
-          inline
-          onChange={(e, data) => {
-            this.setState({
-              selectedLocation3: data.value,
-              selectedLocation4: 0,
-              selectedLocation5: 0
-            });
-            this.props.chooseSpaceAction(location5[selectedLocation5]);
-          }}
-          options={location3}
-          value={selectedLocation3}
-        />
-        <Dropdown
-          inline
-          onChange={(e, data) => {
-            this.setState({
-              selectedLocation4: data.value,
-              selectedLocation5: 0
-            });
-          }}
-          options={location4}
-          value={selectedLocation4}
-        />
-        <Dropdown
-          inline
-          onChange={(e, data) => {
-            console.log("5 changed");
-            this.setState({
-              selectedLocation5: data.value
-            });
-            this.props.chooseSpaceAction(location5[selectedLocation5]);
-          }}
-          options={location5}
-          value={selectedLocation5}
-        />
+            <Dropdown
+              value={selectedLocation2}
+              inline
+              onChange={(e, data) => {
+                console.log("change");
+                this.setState({
+                  selectedLocation2: data.value,
+                  selectedLocation3: 0,
+                  selectedLocation4: 0,
+                  selectedLocation5: 0,
+                  changed: true
+                });
+                this.props.chooseSpaceAction(location5[selectedLocation5]);
+              }}
+              options={location2}
+            />
+            <Dropdown
+              inline
+              onChange={(e, data) => {
+                this.setState({
+                  selectedLocation3: data.value,
+                  selectedLocation4: 0,
+                  selectedLocation5: 0,
+                  changed: true
+                });
+                this.props.chooseSpaceAction(location5[selectedLocation5]);
+              }}
+              options={location3}
+              value={selectedLocation3}
+            />
+            <Dropdown
+              inline
+              onChange={(e, data) => {
+                this.setState({
+                  selectedLocation4: data.value,
+                  selectedLocation5: 0,
+                  changed: true
+                });
+              }}
+              options={location4}
+              value={selectedLocation4}
+            />
+            <Dropdown
+              inline
+              onChange={(e, data) => {
+                console.log("5 changed");
+                this.setState({
+                  selectedLocation5: data.value,
+                  changed: true
+                });
+                this.props.chooseSpaceAction(location5[selectedLocation5]);
+              }}
+              options={location5}
+              value={selectedLocation5}
+            />
+          </div>
+        ) : (
+          <div>No spaces are available for this vehicle type.</div>
+        )}
       </div>
     );
   }
