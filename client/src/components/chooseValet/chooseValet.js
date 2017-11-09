e56362a4ef8eada10c765a879a040616a6b3be15
dannohr <danielnohr@gmail.com>
2017-11-09 09:50:08 -0600

    Increased Valet Name Text Size

--- client/src/components/chooseValet/chooseValet.js
+++ client/src/components/chooseValet/chooseValet.js
@@ -3,38 +3,40 @@ import { connect } from "react-redux";
 import NavBar from "../navBar/navBar";
 import { chooseValetAction, setNavTitleAction } from "../../reducers";
 
-import { Grid, Header, Segment,Button } from "semantic-ui-react";
+import { Grid, Header, Segment, Button } from "semantic-ui-react";
 
 class ChooseValet extends Component {
   constructor(props) {
     super(props);
-    this.props.setNavTitleAction("Home", () =>
-      this.props.history.goBack()
-    );
+    this.props.setNavTitleAction("Home", () => this.props.history.goBack());
     this.state = {
       chosenValet: ""
     };
     this.handleChooseValet = this.handleChooseValet.bind(this);
   }
-  
 
   handleChooseValet(valet) {
     this.props.chooseValetAction(valet);
     this.props.history.push("search");
   }
   render() {
-    console.log('path',this.props.history.location.pathname);
-    const verb = this.props.history.location.pathname
+    console.log("path", this.props.history.location.pathname);
+    const verb = this.props.history.location.pathname;
     const valet = this.state.chosenValet;
     console.log(this.props.employees);
-    const valetList = this.props.employees.filter((employee) => employee.isactive === true)
-    .map(employee => (
-      <Segment
-        onClick={(e, data) => this.setState({ chosenValet: employee.id, valetName: employee.name })}
-      >
-        {employee.name}
-      </Segment>
-    ));
+    const valetList = this.props.employees
+      .filter(employee => employee.isactive === true)
+      .map(employee => (
+        <Segment
+          onClick={(e, data) =>
+            this.setState({
+              chosenValet: employee.id,
+              valetName: employee.name
+            })}
+        >
+          {employee.name}
+        </Segment>
+      ));
     return (
       <div>
         <NavBar />
@@ -42,18 +44,26 @@ class ChooseValet extends Component {
         <Grid padded="vertically" centered>
           <Grid.Row>
             <Header as="h3" color="grey">
-              Who is {verb === '/park/chooseValet'? <span>Parking</span> :<span>Retrieving</span>} this Car?
+              Who is{" "}
+              {verb === "/park/chooseValet" ? (
+                <span>Parking</span>
+              ) : (
+                <span>Retrieving</span>
+              )}{" "}
+              this Car?
             </Header>
           </Grid.Row>
           <Grid.Column width={12}>
             <Segment.Group>{valetList}</Segment.Group>
           </Grid.Column>
           <Grid.Row>
-          {valet 
-          ? 
-          <p>{this.state.valetName}</p>
-          :null}
+            {valet ? (
+              <Header as="h1" color="black">
+                {this.state.valetName}
+              </Header>
+            ) : null}
           </Grid.Row>
+
           <Grid.Row>
             <Button
               color="yellow"
