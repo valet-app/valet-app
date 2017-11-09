fca6ba027fde887e8190a19fa0e094c908023f3a
dannohr <danielnohr@gmail.com>
2017-11-09 09:41:52 -0600

    endpoint updates

--- server/routes/chartCtrl.js
+++ server/routes/chartCtrl.js
@@ -8,5 +8,15 @@ module.exports = {
         res.status(200).json(response);
       })
       .catch(() => res.status(500).send());
+  },
+  getHourlyRetrievals: (req, res, next) => {
+    console.log("getting number retrieved per hour on ", req.query.chartdate);
+    const db = req.app.get("db");
+    db
+      .getHourlyRetrievals(req.query.chartdate)
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(() => res.status(500).send());
   }
 };
